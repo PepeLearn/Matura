e21e2b54899e5babe80ee53921a6ca85f734fda2
unknown <misel.vauk1@gmail.com>
2022-10-16 16:06:18 +0200

    initial

---
+++ Front/Home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="\CSS\Home.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>Home</title>
+</head>
+<body>
+  <header>
+    <nav>
+      <a class="link" href="Store.php">Store</a>
+      <a class="link" href="Account.php">Account</a>
+      <a class="link" href="Account.php">Contact</a>
+      <a class="link" href="Account.php">About us</a>
+    </nav>
+  </header>
+  <section id="head_page">
+    <div id="transparent-black-header-border-up"></div>
+    <div id="right-side-of-header">
+      <img id="head-img" src="/Slike/Ozadje/main.jpg" alt="Ozadje-naslovnice">
+    </div>
+    <div id="left-side-of-header">
+      <div id="transparent-black-header-border-around">
+          <div id="transparent-black-header-border">
+            <h1>Trgovina Tara</h1>
+            <a href="Store.php" class="button">Shop now!</a>
+            <p id="header-text">For the modern woman who wants to feel confident and comfortable. Express your unique style with our stylish clothes. Dressing well doesn't have to be hard – find what you love at our store. You're worth it: shop our collection and feel great about your look.</p>
+          </div>
+      </div>
+    </div>
+  </section>
+  <main>
+    <section id="main_section">
+    </section>
+    <section id="sub_section"> 
+    </section>
+  </main>
+  <footer>
+
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script></body>
+</html>
---
+++ Front/Login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="\CSS\Login.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>Login</title>
+</head>
+<body>
+    <div class="ozadje">
+        <div class="form-ozadje">
+            <h1>Login</h1>
+            <form method="POST">
+                <label for="u">Username</label>
+                <input class="poravnava_user" type="text" name="username" id="u"></br></br>
+                <label" for="g">Password</label>
+                <input class="poravnava_user" type="password" name="password" id="g">
+                <input class="a" type="submit" value="login";></br></br>
+                <p>Dont have an account yet?</p></br>
+                <a class="text-decoration-none link" href="Signup.php">Sign up</a>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
