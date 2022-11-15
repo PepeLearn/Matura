87ff929cd14d7537edcbf9f5319957ecc619562a
nsorgo <nejc.sorgo@gmail.com>
2022-11-15 18:17:32 +0100

    added basic login and auth WIP

--- Backend/database/database.php
+++ Backend/database/database.php
@@ -3,11 +3,26 @@
 <?php
 include "../auth/jwt.php";
 include "connect.php";    
-$user=$_POST["user"];
+$username=$_POST["username"];
 $password=$_POST["password"];
 $sql = "SELECT username,password FROM user WHERE password ='$password'AND user='$username'";
   if ($conn->query($sql))
-   
+  {
+    $payload = array ( // doda username in password k payloadu
+      'password' => $password,
+      'username:' => $username
+    );
+    $generate = new JWT(); 
+    $token = $generate->generate($payload); // generira token z payloadom
+
+  } 
+  else // ce sql ne vrne vnosa, vrne kodo 401 (unauthorised) v http header
+  {
+    header("HTTP/1.1 401 Unauthorized");
+    exit;
+  }
+  
+
 
 
 ?>
