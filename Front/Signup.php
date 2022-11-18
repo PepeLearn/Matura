5036b6015f963c621e07bc89403e93e03ecd7472
nsorgo <nejc.sorgo@gmail.com>
2022-11-18 16:48:03 +0100

    finished basic login

--- Front/Signup.php
+++ Front/Signup.php
@@ -11,6 +11,12 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>Sign up</title>
 </head>
+<script>
+/// vir : https://developer.mozilla.org/en-US/docs/Web/API/Event/type, https://www.w3schools.com/js/js_htmldom_events.asp 
+//document.getElementById("u").addEventListener("keypress", UsernameCheck); // doda event listener na html element "u" (username input) in preveri, ce nekdo vnese keyboard input v polje
+//document.getElementById("g").addEventListener("keypress", PasswordCheck); // doda event listener na html element "g" (username input) in preveri, ce nekdo vnese keyboard input v polje
+///WIP
+</script>
 <body>
     <div class="ozadje">
         <div class="form-ozadje">
@@ -19,6 +25,7 @@
                 <label for="u">Username</label>
                 <input class="poravnava_user" type="text" name="username" id="u"></br></br>
                 <label" for="g">Password</label>
+                <input class="poravnava_user" type="password" name="password" id="g">
                 <input class="poravnava_user" type="password" name="password" id="g">
                 <input class="a" type="submit" value="Sign up";></br></br>
                 <p>Already have an account??</p></br>
