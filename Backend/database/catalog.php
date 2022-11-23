3e71a72d14f25012dbfb96d8f6aae220fccec83d
nsorgo <nejc.sorgo@gmail.com>
2022-11-23 19:21:34 +0100

    updated database

--- Backend/database/catalog.php
+++ Backend/database/catalog.php
@@ -9,7 +9,7 @@ class catalog
         $sql = "SELECT p.ProductName, p.ProductPrice, p.ProductCategory, t.TagName FROM p products,t tags, tp tagToProduct WHERE p.id = tp.id AND t.id = tp.id";
         $result = $conn->query($sql);
         while ($row = mysqli_fetch_assoc($result)) {
-            echo json_encode($row);
+            echo json_encode($row); // vrne vnos v json formatu
         }
     }
 }
