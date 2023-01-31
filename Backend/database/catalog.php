94bac78b58f3e319673edde508fb350c31ca6602
nsorgo <nejc.sorgo@gmail.com>
2023-01-31 19:52:08 +0100

    changed db api from mysqli to PDO

--- Backend/database/catalog.php
+++ Backend/database/catalog.php
@@ -3,31 +3,21 @@ class catalog
 {
 
     public function getCatalog($conn)
-    { // WIP
-        $sql = "SELECT p.ProductName, p.ProductPrice, p.ProductCategory, t.TagName FROM products p, tags t,tagToProduct tp WHERE p.id = tp.ProductID AND t.id = tp.TagID";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            $i = 1;
-            while ($row = mysqli_fetch_assoc($result)) {
-                if ($i == 1)
-                {
-                    $row2 = $row;
-                }
-                if ($row2["ProductName"] == $row["ProductName"]) // preveri ce je originalni vnos enak novemu
-                {
-                    array_merge($row2, $row);
-                    //$row2["TagName"] = array($row2["TagName"],$row["TagName"]); //doda tag v original vnos
-                }
-                else { // ce ni vrne json
-                    echo json_encode($row2); // vrne vnos v json formatu
-                    echo ("<br  >");
-                    $row2 = $row;
-                }
-               
-                $i++;
-            }
+    { // WIP(
+
+        $sql = "SELECT p.ProductName, p.ProductPrice, p.ProductCategory, GROUP_CONCAT(t.tagName SEPARATOR ', ') AS 'tags' FROM products p, tags t,tagToProduct tp WHERE p.id = tp.ProductID AND t.id = tp.TagID GROUP BY productName; ";
+        $statement = $conn->prepare($sql);
+        $statement->execute();
+        while ($row = $statement->fetch()) {
+            $tags = explode(",", $row["tags"]);
+            $product =  array(
+              "Name" => $row["ProductName"],
+              "Price" => $row["ProductPrice"],
+              "Category" => $row["ProductCategory"],
+              "Tags" => $tags 
+            );
+            echo  json_encode($product);
         }
-        echo json_encode($row2);
         return true;
     }
 }
