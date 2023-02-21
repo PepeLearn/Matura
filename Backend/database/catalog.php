5169104ce51eecfd3b9648cf9f9c543b993cd61a
nsorgo <nejc.sorgo@gmail.com>
2023-02-21 20:55:43 +0100

    initial db overhaul

--- Backend/database/catalog.php
+++ Backend/database/catalog.php
@@ -4,7 +4,7 @@ class catalog
 
     public function getCatalog($conn)
     { // WIP
-        $sql = "SELECT p.ProductName, p.ProductPrice, p.ProductCategory, GROUP_CONCAT(t.tagName SEPARATOR ', ') AS 'tags' FROM products p, tags t,tagToProduct tp WHERE p.id = tp.ProductID AND t.id = tp.TagID GROUP BY productName; ";
+        $sql = "SELECT p.productName, p.productPrice, p.productCategory, GROUP_CONCAT(t.tagName SEPARATOR ', ') AS 'tags' FROM product p, tags t,tagtoproduct tp WHERE p.id = tp.productID AND t.id = tp.TagID GROUP BY productName; ";
         $statement = $conn->prepare($sql);
         $statement->execute();
         while ($row = $statement->fetch()) { // hardcoded, ker drugace nena vredi dela
