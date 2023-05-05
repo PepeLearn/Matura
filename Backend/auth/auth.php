328f1c2da245a4592a0f6bebb361ee0a5e58e0e2
nsorgo <nejc.sorgo@gmail.com>
2023-05-05 14:43:48 +0200

    changed project structure, moved backend

--- Backend/auth/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "jwt.php";
-$jwt = $_GET["token"];
-$auth= new JWT();
-if ($auth->is_valid($token) ) /// prever ce je JWT pravilen
-    header("HTTP/1.1 401 Unauthorized");
-    else
-?>
