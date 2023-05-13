87e5c64c235bd605039816d393ae4283ac7070ed
PepeLearn <misel.vauk1@gmail.com>
2023-05-13 16:59:38 +0200

    cart dela ampak rabi fix + quantity add and remove function

--- user-page/src/pages/account.jsx
+++ user-page/src/pages/account.jsx
@@ -1,14 +1,14 @@
-import Header from '../components/header'
-import Footer from '../components/footer'
-import { useState, useEffect } from "react"
-import Cookies from 'js-cookie';
+import Header from "../components/header";
+import Footer from "../components/footer";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
 
 const Account = () => {
   const [AccountData, setAccoutData] = useState({
-    username: 'pepe',
-    email: 'pepe@gmail.com'
+    username: "pepe",
+    email: "pepe@gmail.com",
   });
-  const [auth,setAuth] = useState(Cookies.get("authorization") ) 
+  const [auth, setAuth] = useState(Cookies.get("authorization"));
   const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileChange = (event) => {
@@ -17,77 +17,95 @@ const Account = () => {
 
   const handleUpload = () => {
     const formData = new FormData();
-    formData.append('pfp', selectedFile);
+    formData.append("pfp", selectedFile);
 
-    fetch('http://127.0.0.1/matura-backend/database/database.php?getAccountData=true', {
-      method: 'POST',
-      body: formData,
-    })
+    fetch(
+      "http://127.0.0.1/matura-backend/database/database.php?getAccountData=true",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
       .then((response) => {
-        alert("success")
+        alert("success");
       })
       .catch((error) => {
-        alert("failed to upload picture")
+        alert("failed to upload picture");
       });
   };
-  
 
   useEffect(() => {
-    fetch("http://127.0.0.1/matura-backend/database/database.php?getAccountData=true", {
-      method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': auth
-      },
-    })
+    fetch(
+      "http://127.0.0.1/matura-backend/database/database.php?getAccountData=true",
+      {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: auth,
+        },
+      }
+    )
       .then((data) => data.json())
       .then((data) => {
         setAccoutData(data);
       })
       .catch((error) => {
-        console.error('Error:', error);
-
+        console.error("Error:", error);
       });
-  }, []) //vsakic ko se filter changa fetchne producte in jih spremeni  
-  const handleLogout = () => { // to dela
+  }, []); //vsakic ko se filter changa fetchne producte in jih spremeni
+  const handleLogout = () => {
+    // to dela
     Cookies.remove("authorization");
     console.log("logout pressed");
     window.location.replace("/");
-  }
+  };
   return (
-    <div>
+    <div className="h-auto">
       <Header />
-      <div className='h-screen'>
-        <div>
-          <h1 className='text-center text-black font-serif text-5xl pt-20'>Profile</h1>
-          <h2 className='text-center text-black font-serif text-2xl pt-10'>
-            <div>{AccountData.username}</div>
-            <div>{AccountData.email}</div>
-            <div>{AccountData.phoneNumber}</div>
-            <div>{AccountData.id}</div>
-          </h2>
-          <div>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Upload PFP</button>
+      <div className="h-screen mb-20">
+        <div className="h-60 ml-10 mr-10 mt-10 -mb-20 bg-yellow-500"></div>
+        <div className="flex justify-center mt-20">
+          <div className="w-[90rem] h-[46.5rem] -m-20  bg-white shadow-2xl">
+            <div className="m-10 text-2xl	border-b-2 pb-5 border-gray-300">
+              Account Settings
+            </div>
+            <div className="flex justify-center mt-10">
+              <div className="h-[35rem] w-[80rem]">
+                <div>
+                  <div className="float-right">
+                    <div className="bg-gray-100">
+                      <input type="file" onChange={handleFileChange} />
+                      <button onClick={handleUpload}>Upload PFP</button>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="text-3xl	m-5">Username:</div>
+                    <div className="text-2xl	m-5 border-2 border-gray-300 w-1/4">
+                      @{AccountData.username}
+                    </div>
+                    <div className="text-3xl	m-5">E-mail:</div>
+                    <div className="text-2xl	m-5 border-2 border-gray-300 w-1/4">
+                      {AccountData.email}
+                    </div>
+                    <div className="text-3xl	m-5">Phone number:</div>
+                    <div className="text-2xl	m-5 border-2 border-gray-300 w-1/4">
+                      070 556 553
+                    </div>
+                  </div>
+                  <div>
+                    <div className="text-3xl	m-5 pt-20 mt-20 ">
+                      <button onClick={handleLogout}>Log out</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className='h-screen flex flex-col'>
-          <div className='h-1/6'>
-            <h1 className='text-left ml-20 mt-10 text-black font-serif text-xl font-bol drop-shadow-lg shadow-black'>Order History</h1>
-          </div>
-          <div>
-            {
-
-            }
-          </div>
-        </div>
-      </div>
-      <div>
-        <button onClick={handleLogout}>Log out</button>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Account;
--- user-page/src/pages/cart.jsx
+++ user-page/src/pages/cart.jsx
@@ -1,24 +1,67 @@
-import Header from '../components/header'
-import Footer from '../components/footer'
-import { useState, useEffect } from 'react'
-import { json } from 'react-router'
-import Cookies from 'js-cookie'
+import Header from "../components/header";
+import Footer from "../components/footer";
+import { useState, useEffect } from "react";
+import { json } from "react-router";
+import Cookies from "js-cookie";
 
 const Cart = () => {
-  const [items, setItems] = useState(JSON.parse(Cookies.get("cart")))
+  const [items, setItems] = useState(JSON.parse(Cookies.get("cart")));
   console.log(items);
   return (
     <div>
-      bruh
-      <div>
-        {
-          items.map((item, i) => ( // prikaze barve produktov. 
-            <div key={i}>{item.color}</div>
-          ))
-        }
+      <Header />
+      <div className="bg-white">
+        <h1 className="text-left m-20 text-3xl">Cart</h1>
       </div>
+      <div className="h-auto">
+        <div className="w-50% bg-white p-10 m-20 h-48 shadow-2xl border-2 border-black flex flex-row justify-between">
+          {items &&
+            items.map((item) => (
+              <div className="flex flex-row">
+                <div className="flex flex-col justify-center">
+                  <img
+                    className="w-24 h-24 object-contain"
+                    src={`http://127.0.0.1/matura-backend/products/images/${item.id}.jpg`}
+                    alt="Product Image"
+                  />
+                </div>
+                <div className="flex flex-col justify-center ml-10">
+                  {item.name}
+                </div>
+                <div className="flex flex-col justify-center">
+                  <button className="text-center border m-4 p-1 w-1/2 border-gray-800">
+                    +
+                  </button>
+                  <div className="text-center border p-1 border-gray-800">
+                    {item.quantity}
+                  </div>
+                  <button className="text-center border m-4 p-1 w-1/2 border-gray-800">
+                    -
+                  </button>
+                </div>
+                <div className="flex flex-col justify-center">
+                  <div className="text-center">Remove from cart</div>
+                  <button className="text-center border m-7 p-1 w-1/2 border-gray-800">
+                    -
+                  </button>
+                </div>
+                <div className="flex flex-col justify-center">
+                  <div>Price:</div>
+                  <div>100€</div>
+                </div>
+              </div>
+            ))}
+          <div className="m-20 text-right">
+            <div className="mr-10 mb-10 text-2xl">Total: 1000€</div>
+            <a href="" className="bg-orange-400 p-5">
+              Proceed to checkout
+            </a>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
