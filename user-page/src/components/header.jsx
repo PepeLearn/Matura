cf039126fc684e54cf10867333470a9c1e2ff2e9
PepeLearn <misel.vauk1@gmail.com>
2023-05-16 16:34:24 +0200

    oof

--- user-page/src/components/header.jsx
+++ user-page/src/components/header.jsx
@@ -11,8 +11,8 @@ const Header = () => {
   const num_of_items = items.length;
 
   return (
-    <div className="flex justify-between p-5 shadow-2xl">
-      <div>
+    <div className="flex flex-wrap justify-between p-5 shadow-2xl">
+      <div className="w-full md:w-auto mb-5 md:mb-0">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Service_mark.svg/2560px-Service_mark.svg.png"
           alt=""
@@ -20,32 +20,37 @@ const Header = () => {
           width={100}
         />
       </div>
-      <nav>
-        <a className="m-5 text-lg" href="/">
+      <nav className="w-full md:w-auto mb-5 md:mb-0">
+        <a className="block md:inline-block m-2 md:m-5 text-lg" href="/">
           Home
         </a>
-        <a className="m-5 text-lg" href="/catalog">
+        <a className="block md:inline-block m-2 md:m-5 text-lg" href="/catalog">
           Store
         </a>
-        <a className="m-5 text-lg" href="/#contact-form">
-          contact us
+        <a
+          className="block md:inline-block m-2 md:m-5 text-lg"
+          href="/#contact-form"
+        >
+          Contact Us
         </a>
       </nav>
-      <div>
+      <div className="flex items-center">
         {Cookies.get("authorization") ? (
-          <a className="m-5 text-lg" href="/account">
-            My profile
+          <a className="m-2 md:m-5 text-lg" href="/account">
+            My Profile
           </a>
         ) : (
-          <a className="m-5 text-lg" href="/login">
+          <a className="m-2 md:m-5 text-lg" href="/login">
             Login
           </a>
         )}
-        <a href="./cart" className="bg-amber-500 p-3 rounded-lg">
-          Cart
-        </a>
-        <div className="absolute top-10 right-3.5 border-2 border-amber-500 text-xs bg-white p-1 rounded-full">
-          {items ? num_of_items : 0}
+        <div className="relative">
+          <a href="./cart" className="bg-amber-500 p-3 rounded-lg">
+            Cart
+          </a>
+          <div className="absolute top-6 right-0 -mt-1 -mr-1 bg-white border-2 border-amber-500 text-xs px-1 rounded-full">
+            {items ? num_of_items : 0}
+          </div>
         </div>
       </div>
     </div>
